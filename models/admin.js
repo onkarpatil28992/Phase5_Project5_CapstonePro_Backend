59e9451121fc381af3b4fc91087d6e6b53d7710a
Onkar Patil <onkar.patil@sakec.ac.in>
2022-06-13 09:49:22 +0530

    from local

---
+++ models/admin.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const AdminSchema = new Schema({
+  adminID: String,
+  password: String
+});
+
+
+module.exports = mongoose.model("Admin", AdminSchema);
+
+
+
+
